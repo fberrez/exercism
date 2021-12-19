14ea4a8e58a3b64ccec0f6c21504f400d6c10763
fberrez <bf0c6670-0084@protonmail.com>
2021-12-19 20:26:49 +0100

    refactor(typescript): improve `bob`

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,54 +1,16 @@
-const SURE: string = 'Sure.';
-const CHILL: string = 'Whoa, chill out!';
-const CALM: string = "Calm down, I know what I'm doing!";
-const FINE: string = 'Fine. Be that way!';
-const WHATEVER: string = 'Whatever.';
-
 function isQuestion(message: string): boolean {
-    return message[message.length - 1] === '?';
+    return message.endsWith('?');
 }
 
 function isYelling(message: string): boolean {
-    return message === message.toUpperCase();
-}
-
-function isAnything(message: string): boolean {
-    const regexp = /[a-zA-Z]/g;
-    return !regexp.test(message);
-}
-
-function isFigureOnly(message: string): boolean {
-    const regexp = /[0-9]/g;
-    const letters = /[a-zA-Z]/g;
-    return regexp.test(message) && !letters.test(message);
+    return message === message.toUpperCase() && /[a-zA-Z]/.test(message);
 }
 
 export function hey(message: string): string {
     const trimmedMsg = message.trim();
-    if (message.trim().length === 0 ) {
-        return FINE;
-    }
-    
-    if (isFigureOnly(trimmedMsg) && isQuestion(trimmedMsg)) {
-        return SURE;
-    }
-
-    if (isFigureOnly(trimmedMsg)) {
-        return WHATEVER;
-    }
-
-    if (isYelling(trimmedMsg) && isQuestion(trimmedMsg)) {
-        return CALM;
-    }
-
-    if (isQuestion(trimmedMsg)) {
-        return SURE;
-    }
-
-    if (isYelling(trimmedMsg)) {
-        return CHILL;
-    }
-
-
-    return WHATEVER;
+    if (!trimmedMsg) { return 'Fine. Be that way!' }
+    if (isYelling(trimmedMsg) && isQuestion(trimmedMsg)) { return "Calm down, I know what I'm doing!" }
+    if (isYelling(trimmedMsg)) { return 'Whoa, chill out!' }
+    if (isQuestion(trimmedMsg)) { return 'Sure.' }
+    return 'Whatever.';
 }
